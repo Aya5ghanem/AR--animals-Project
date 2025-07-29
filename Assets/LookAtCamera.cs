e066f2b38cf3a4bb9abf60285b8af340c80a0996
Aya5ghanem <104633565+Aya5ghanem@users.noreply.github.com>
2025-07-29 16:27:43 +0300

    Look At script (model look at camera.)

---
+++ Assets/LookAtCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    void Update()
+    {
+        // خلّي العنصر ده يبص على الكاميرا الرئيسية
+        if (Camera.main != null)
+        {
+            transform.LookAt(Camera.main.transform);
+            transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + 180f, 0);
+
+        }
+    }
+}
